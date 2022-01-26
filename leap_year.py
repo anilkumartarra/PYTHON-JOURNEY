ef3c173ff2849f99562af0419a64ed7f50033100
Tarra Anil Kumar <91973092+anilkumartarra@users.noreply.github.com>
2022-01-26 18:43:56 +0530

    Create leap_year.py

---
+++ leap_year.py
@@ -0,0 +1,8 @@
+t=int(input())
+for i in range(t):
+    year=int(input())
+    if year%400 == 0 or year%4 == 0 and year%100 != 0 :
+        print("Leap Year")
+    else:
+        print("Not Leap Year")    
+    print()   
